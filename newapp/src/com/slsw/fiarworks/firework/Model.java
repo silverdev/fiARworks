336ff9198af865db4192ee55a1eb7dab04c4aee5
Nick <nickljeffrey@gmail.com>
2013-11-09 23:11:16 -0600

    Meet the newapp, same as the oldapp

---
+++ newapp/src/com/slsw/fiarworks/firework/Model.java
@@ -0,0 +1,22 @@
+package com.slsw.fiarworks.firework;
+
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.ByteOrder;
+
+public class Model
+{
+	public FloatBuffer geometryBuffer;
+	public int num_geometry;
+
+	Model(float[] geometry)
+	{
+		num_geometry = geometry.length;
+		System.out.println("Size of allocation: " + geometry.length * 4);
+		ByteBuffer bb = ByteBuffer.allocateDirect(geometry.length * 4);	//4 bytes per float
+		bb.order(ByteOrder.nativeOrder());
+		geometryBuffer = bb.asFloatBuffer();
+		geometryBuffer.put(geometry);
+		geometryBuffer.position(0);
+	}
+}
---
+++ newapp/src/com/slsw/fiarworks/firework/Spark.java
@@ -0,0 +1,43 @@
+package com.slsw.fiarworks.firework;
+import java.util.ArrayList;
+
+public class Spark
+{
+	protected Vec3 position;
+	private Vec3 velocity;
+	private int type;
+	private int age;
+	Spark(Vec3 pos, Vec3 vel, int t)
+	{
+		position = pos;
+		velocity = vel;
+		type = t;
+
+	}
+	ArrayList<Spark> update()
+	{
+		ArrayList<Spark> return_list = new ArrayList<Spark>();
+		position = position.add(velocity);
+		if(type == 0)
+		{
+			if(position.z > 5.0f)
+			{
+				//Explode
+				for(int i = 0; i < 100; i++)
+				{
+					Spark new_spark = new Spark(new Vec3(position), Vec3.random_velocity(0.04f), 1);
+					return_list.add(new_spark);
+				}
+			}
+			else
+			{
+				return_list.add(this);
+			}
+		}
+		else if(type == 1)
+		{
+			return_list.add(this);
+		}
+		return return_list;
+	}
+}
