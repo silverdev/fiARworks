ea95360ab672bbeb653ea702d3111b398d0e1fc7
Nick <nickljeffrey@gmail.com>
2013-12-02 00:43:58 -0600

    Receiving camrea frames, no longer receiving rotation information??

--- newapp/src/com/slsw/fiarworks/CameraPreview.java
+++ newapp/src/com/slsw/fiarworks/CameraPreview.java
@@ -5,115 +5,141 @@ package com.slsw.fiarworks;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.hardware.Camera.PreviewCallback;
+import android.hardware.Camera.Size;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import com.slsw.fiarworks.firework.GLRenderer;
-import com.slsw.fiarworks.masker.AlphaMake;
 
-public class CameraPreview extends SurfaceView implements
-		SurfaceHolder.Callback, PreviewCallback, SensorEventListener {
-	private SurfaceHolder mHolder;
-	public volatile Camera mCamera;
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback, SensorEventListener {
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+    private String TAG = "CameraPreview";
+    private Context context;
 	private float[] mRotVec;
 	private float[] mRotOld;
-	public volatile boolean sendBitmap;
 	private byte[][] mask = null;
 	public GLRenderer mRenderer;
+	public volatile boolean sendBitmap;
 
-	public CameraPreview(Context context, Camera camera, GLRenderer renderer) {
-		super(context);
-		mCamera = camera;
-		mHolder = getHolder();
-		mHolder.addCallback(this);
-		sendBitmap=true;
-		mRenderer=renderer;
-	}
+    @SuppressWarnings("deprecation")
+    public CameraPreview(Context context, Camera camera, GLRenderer renderer) {
+    super(context);
+    mCamera = camera;
+    this.context = context;
 
-	public void surfaceCreated(SurfaceHolder holder) {
-		try {
-			if (mCamera != null) {
-				mCamera.setPreviewDisplay(holder);
-				mCamera.startPreview();
-			}else { System.out.println("CAMERA NULL IN SURFACECREATED"); }
-		} catch (IOException e) {
-			Log.d(VIEW_LOG_TAG,
-					"Error setting camera preview: " + e.getMessage());
-		}
-	}
+    // Install a SurfaceHolder.Callback so we get notified when the
+    // underlying surface is created and destroyed.
+    mHolder = getHolder();
+    // deprecated setting, but required on Android versions prior to 3.0
+    mHolder.addCallback(this);
+    mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	sendBitmap=true;
+	mRenderer=renderer;
+    }
 
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		if (mCamera != null) {
-			//mCamera.stopPreview();
-			//mCamera.release();
-		}
-	}
+    public void surfaceCreated(SurfaceHolder holder) {
+    // The Surface has been created, now tell the camera where to draw the preview.
+    try {
+        mCamera.setPreviewDisplay(holder);
+        mCamera.startPreview();
 
-	public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-		// Rotation not enabled, possibly will be in the future
+    } catch (NullPointerException e) {
+        Log.d(TAG, "Error setting camera preview - nullpointerexception: " + e.getMessage());
+    } catch (IOException e) {
+        Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+    }
+    }
 
-		if (mHolder.getSurface() == null) {
-			// preview surface does not exist
-			return;
-		}
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    // empty. Take care of releasing the Camera preview in your activity.
+    }
 
-		// stop preview before making changes
-		try {
-			mCamera.stopPreview();
-		} catch (Exception e) {
-			// camera does not exist or is not previewing
-		}
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+    // If your preview can change or rotate, take care of those events here.
+    // Make sure to stop the preview before resizing or reformatting it.
 
-		// Once rotation is enabled, code for that will go here
+    if (mHolder.getSurface() == null){
+      // preview surface does not exist
+      return;
+    }
 
-		// start preview with new settings
-		try {
-			mCamera.setPreviewDisplay(mHolder);
-			mCamera.startPreview();
+    // stop preview before making changes
+    try {
+        mCamera.stopPreview();
+    } catch (Exception e){
+      // ignore: tried to stop a non-existent preview
+    }
 
-		} catch (Exception e) {
-			Log.d(VIEW_LOG_TAG,
-					"Error starting camera preview: " + e.getMessage());
-		}
-	}
+    // set preview size and make any resize, rotate or
+    // reformatting changes here
 
-	/*
-	 * Surface or settings changed
-	 */
-	public void setCamera(Camera c) {
-		mCamera = c;
-	}
+    // start preview with new settings
+    try {
+        Parameters parameters = mCamera.getParameters();
 
-	/*
-	 * This function is called for every new preview frame. This is the data that we are receiving from camera!
-	 * Pass this along to the Overlay, but first compress it down to a reasonable size and convert it into a bitmap.
-	 */
-	@Override
-	public void onPreviewFrame(byte[] data, Camera c) { 
-		if(sendBitmap){
-	        Camera.Parameters p = c.getParameters();
-	        int width = p.getPreviewSize().width;
-	        int height = p.getPreviewSize().height;
-	        
-	        sendBitmap=true;
-	        
-	        mask = AlphaMake.makeSimpleMask(data, height, width, mRotVec);
-	        mRotOld=mRotVec.clone();
-	        mRenderer.setTextures(data, mask);
-	        mRenderer.setChange(changeInRot());
-		}
-	}
+        List<Size> sizes = parameters.getSupportedPreviewSizes();
+        Size optimalSize = getOptimalPreviewSize(sizes, w, h);
+        parameters.setPreviewSize(optimalSize.width, optimalSize.height);
 
-	@Override
+        if (context.getPackageManager().hasSystemFeature("android.hardware.camera.autofocus"))
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+
+        mCamera.setParameters(parameters);
+
+        mCamera.setPreviewCallback((PreviewCallback) context);
+        mCamera.setPreviewDisplay(mHolder);
+        mCamera.startPreview();
+
+    } catch (Exception e){
+        Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+    }
+    }
+
+    private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
+    final double ASPECT_TOLERANCE = 0.05;
+    double targetRatio = (double) w / h;
+    if (sizes == null) return null;
+
+    Size optimalSize = null;
+    double minDiff = Double.MAX_VALUE;
+
+    int targetHeight = h;
+
+    // Try to find an size match aspect ratio and size
+    for (Size size : sizes) {
+        double ratio = (double) size.width / size.height;
+        if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+        if (Math.abs(size.height - targetHeight) < minDiff) {
+            optimalSize = size;
+            minDiff = Math.abs(size.height - targetHeight);
+        }
+    }
+
+    // Cannot find the one match the aspect ratio, ignore the requirement
+    if (optimalSize == null) {
+        minDiff = Double.MAX_VALUE;
+        for (Size size : sizes) {
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+    }
+    return optimalSize;
+    }
+    
+    @Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 		// TODO Auto-generated method stub
 		
@@ -121,11 +147,11 @@ public class CameraPreview extends SurfaceView implements
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		//System.err.println(Arrays.toString(event.values));
+		System.err.println(Arrays.toString(event.values));
 		if(event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR){
 			//Update rotation and position vector
 			mRotVec = event.values;
-			mRenderer.setChange(changeInRot());
+			if(mRenderer != null) mRenderer.setChange(changeInRot());
 		}
 	}
 	
@@ -138,6 +164,4 @@ public class CameraPreview extends SurfaceView implements
 		return angleChange;
 		
 	}
-	
-	
 }
