d7ac4bc6efd7c8a6c97bfce1e18e518f5ee0bfb2
echuber2 <echuber2@illinois.edu>
2013-12-04 14:19:30 -0600

    added flag to prevent app from sleeping

--- newapp/src/com/slsw/fiarworks/CameraActivity.java
+++ newapp/src/com/slsw/fiarworks/CameraActivity.java
@@ -18,6 +18,7 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.view.WindowManager;
 
 import com.slsw.fiarworks.firework.GLRenderer;
 import com.slsw.fiarworks.masker.AlphaMake;
@@ -34,6 +35,7 @@ public class CameraActivity extends Activity implements Camera.PreviewCallback, 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
+	    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	    setContentView(R.layout.main_layout);
 	    
 	
